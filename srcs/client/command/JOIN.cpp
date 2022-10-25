6e4071b3bb60bff73b6dfdc6f78239360b5538e0
Tosba74 <bastm.ua42@gmail.com>
2022-10-25 15:11:41 +0200

    modif JOIN

--- srcs/client/command/JOIN.cpp
+++ srcs/client/command/JOIN.cpp
@@ -6,7 +6,7 @@
 /*   By: bmangin <bmangin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:27:51 by emenella          #+#    #+#             */
-/*   Updated: 2022/10/24 23:47:40 by bmangin          ###   ########lyon.fr   */
+/*   Updated: 2022/10/25 15:10:21 by bmangin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ JOIN::~JOIN() {}
 int JOIN::execute(Client &clicli, std::vector<std::string> args)
 {
 	//int ret = AuthenticationCommand::execute(clicli, begin, end);
-	int ret = 1;
-    if (ret == 1) {
+    if (clicli->getRegister() == true) {
         _serv->createChannel(args[1]);
         _serv->joinChannel(args[1], clicli);
     } else {
